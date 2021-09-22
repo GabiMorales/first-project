e67f545a7878cb7bc1186dc94979db7f8dd4d423
GabiMorales <gabrimorales381@gmail.com>
2021-09-22 17:06:30 -0300

    commit 3

--- src/database/migrations/1632340465936-init.ts
+++ src/database/migrations/1632340465936-init.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class init1632337222467 implements MigrationInterface {
-    name = 'init1632337222467'
+export class init1632340465936 implements MigrationInterface {
+    name = 'init1632340465936'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tarea" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "completed" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_52df0f8fc74f81d0531ad890f0e" PRIMARY KEY ("id"))`);
